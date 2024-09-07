50bc6d152ae3cbc94261f5257e2af5998f71a0f1
dongmingyan <dongmingyan01@gmail.com>
2024-09-07 15:32:07 +0800

    add admin 表

---
+++ internal/model/entity/admin.go
@@ -0,0 +1,20 @@
+// =================================================================================
+// Code generated and maintained by GoFrame CLI tool. DO NOT EDIT.
+// =================================================================================
+
+package entity
+
+import (
+	"github.com/gogf/gf/v2/os/gtime"
+)
+
+// Admin is the golang structure for table admin.
+type Admin struct {
+	Id        int         `json:"id"        orm:"id"         description:""`
+	Name      string      `json:"name"      orm:"name"       description:"姓名"`
+	Password  string      `json:"password"  orm:"password"   description:"密码"`
+	IsAdmin   int         `json:"isAdmin"   orm:"is_admin"   description:"是否为超级管理员"`
+	CreatedAt *gtime.Time `json:"createdAt" orm:"created_at" description:""`
+	UpdatedAt *gtime.Time `json:"updatedAt" orm:"updated_at" description:""`
+	DeletedAt *gtime.Time `json:"deletedAt" orm:"deleted_at" description:"是否删除"`
+}
