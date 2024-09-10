d4984ea3024acc2f95d26a64b337b7b45245d745
dongmingyan <dongmingyan01@gmail.com>
2024-09-10 09:43:16 +0800

    update

--- internal/logic/playground/playground.go
+++ internal/logic/playground/playground.go
@@ -4,7 +4,6 @@ import (
 	"gf-demo/internal/model/entity"
 	"gf-demo/internal/service"
 
-	"github.com/gogf/gf/v2/database/gdb"
 	"github.com/gogf/gf/v2/frame/g"
 )
 
@@ -27,30 +26,32 @@ type UserInfo struct {
 
 // 这是我自己定义的一个练写方法
 func (s *sPlayground) Do() {
-	// 定义用户列表
-	var users []UserInfo
-	// 查询用户基础数据
-	// SELECT * FROM `user`
-	err := g.Model("user").ScanList(&users, "User")
-	if err != nil {
-		panic(err)
-	}
-	// 查询用户详情数据
-	// SELECT * FROM `user_detail` WHERE `uid` IN(1,2)
-	err = g.Model("user_detail").
-		Where("user_id", gdb.ListItemValuesUnique(users, "User", "Id")).
-		ScanList(&users, "UserDetail", "User", "user_id:id")
-	if err != nil {
-		panic(err)
-	}
-	// 查询用户学分数据
-	// SELECT * FROM `user_scores` WHERE `uid` IN(1,2)
-	err = g.Model("user_scores").
-		Where("user_id", gdb.ListItemValuesUnique(users, "User", "Id")).
-		ScanList(&users, "UserScores", "User", "user_id:id")
-
-	g.Dump("=======当前users:", users)
-	if err != nil {
-		panic(err)
-	}
+	g.Model("user u").LeftJoin("user_detail ud", "u.id=ud.user_id").Fields("u.*,ud.address").Where("u.id", 1).One()
 }
+
+// 定义用户列表
+// var users []UserInfo
+// // 查询用户基础数据
+// // SELECT * FROM `user`
+// err := g.Model("user").ScanList(&users, "User")
+// if err != nil {
+// 	panic(err)
+// }
+// // 查询用户详情数据
+// // SELECT * FROM `user_detail` WHERE `uid` IN(1,2)
+// err = g.Model("user_detail").
+// 	Where("user_id", gdb.ListItemValuesUnique(users, "User", "Id")).
+// 	ScanList(&users, "UserDetail", "User", "user_id:id")
+// if err != nil {
+// 	panic(err)
+// }
+// // 查询用户学分数据
+// // SELECT * FROM `user_scores` WHERE `uid` IN(1,2)
+// err = g.Model("user_scores").
+// 	Where("user_id", gdb.ListItemValuesUnique(users, "User", "Id")).
+// 	ScanList(&users, "UserScores", "User", "user_id:id")
+
+// g.Dump("=======当前users:", users)
+// if err != nil {
+// 	panic(err)
+// }
